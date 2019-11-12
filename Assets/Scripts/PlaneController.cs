7045ade5f95cbe9fc2c35c642344d7280e9ae1b9
Justin Hahn <43224702+HahnJustin@users.noreply.github.com>
2019-11-12 12:58:56 +0100

    i am iron man
    commenting on git, yes i can

--- Assets/Scripts/PlaneController.cs
+++ Assets/Scripts/PlaneController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PlaneController : MonoBehaviour
 {
+    public GameObject ball;
+    public GameObject downwardPointer;
+    public LayerMask rayCastLayerMask;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
+    //Get to move parent empty with child ball
     void Update()
     {
-        
+        RaycastHit hit;
+        if (Physics.Raycast(downwardPointer.transform.position, -Vector3.up, out hit, Mathf.Infinity, rayCastLayerMask))
+        {
+            float offsetDistance = hit.distance;
+            Debug.DrawLine(downwardPointer.transform.position, hit.point, Color.cyan);
+        }
     }
 }
