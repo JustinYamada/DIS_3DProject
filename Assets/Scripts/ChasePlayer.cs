36906a55f46368a3915d3f31a23c1c07b160d3a5
Owen <owengoldthwaite@gmail.com>
2019-11-19 19:49:43 +0100

    Battle Cubes

---
+++ Assets/Scripts/ChasePlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChasePlayer : MonoBehaviour
+{
+    public float speed = 2;
+    public GameObject player;
+
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = player.transform.position;
+    }
+}
