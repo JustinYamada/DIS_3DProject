8efe1b2eae5e3238d70206f0588b3f26475db377
Justin <yamadaj@carleton.edu>
2019-12-03 10:10:18 +0100

    creating new level

---
+++ Assets/Scripts/HammerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HammerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Rigidbody rb;
+    public int xRot;
+    public int yRot;
+    public int zRot = 90;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(xRot, yRot, zRot) * Time.deltaTime);
+    }
+}
