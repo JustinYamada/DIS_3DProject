b6391a9f143f1a05e8e72a1113e4a24153d0054e
Justin Hahn <43224702+HahnJustin@users.noreply.github.com>
2019-12-06 11:48:58 +0100

    RotatorShop Push

--- Assets/Scripts/RotatorShop.cs
+++ Assets/Scripts/RotatorShop.cs
@@ -9,35 +9,76 @@ public class RotatorShop : MonoBehaviour
     public int xRotation;
     public int yRotation;
     public int zRotation;
-
     public float pointerY;
     public int itemId;
+    [HideInInspector] public int price;
 
     public TextMeshProUGUI priceText;
 
-    //XD
-    void Update()
+    public void Update()
     {
         transform.Rotate(new Vector3(xRotation, yRotation, zRotation) * Time.deltaTime);
     }
-
-    private void Start()
+    public void Start()
     {
+
+
         if (itemId == 0)
         {
-            priceText.text = singletonGameManager.Instance.speedItemPrice + " \n Bananas";
+            price = singletonGameManager.Instance.speedItemPrice;
         }
         else if (itemId == 1)
         {
-            priceText.text = singletonGameManager.Instance.phaseItemPrice + " \n Bananas";
+            price = singletonGameManager.Instance.phaseItemPrice;
         }
         else if (itemId == 2)
         {
-            priceText.text = singletonGameManager.Instance.slowItemPrice + " \n Bananas";
+            price = singletonGameManager.Instance.slowItemPrice;
         }
         else
         {
-            priceText.text = singletonGameManager.Instance.jumpItemPrice + " \n Bananas";
+            price = singletonGameManager.Instance.jumpItemPrice;
+        }
+        priceText.text = price + " \n Bananas";
+    }
+
+    public void Buy(int num)
+    {
+        if (itemId == 0)
+        {
+            singletonGameManager.Instance.buySpeedItems(num);
+        }
+        else if (itemId == 1)
+        {
+            singletonGameManager.Instance.buyPhaseItem(num);
+        }
+        else if (itemId == 2)
+        {
+            singletonGameManager.Instance.buySlowItem(num);
+        }
+        else
+        {
+            singletonGameManager.Instance.buyJumpItem(num);
+        }
+    }
+
+    public void Upgrade(int num)
+    {
+        if (itemId == 0)
+        {
+            singletonGameManager.Instance.levelUpSpeedItem(num);
+        }
+        else if (itemId == 1)
+        {
+            singletonGameManager.Instance.levelUpPhaseItem(num);
+        }
+        else if (itemId == 2)
+        {
+            singletonGameManager.Instance.levelUpSlowItem(num);
+        }
+        else
+        {
+            singletonGameManager.Instance.levelUpJumpItem(num);
         }
     }
 }
