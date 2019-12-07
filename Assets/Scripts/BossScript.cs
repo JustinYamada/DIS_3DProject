00bfd7a3feffc84cb0b15258bf5da23f8a4347e2
Justin <yamadaj@carleton.edu>
2019-12-07 14:58:18 +0100

    spiral level is now complete

--- Assets/Scripts/BossScript.cs
+++ Assets/Scripts/BossScript.cs
@@ -6,15 +6,36 @@ public class BossScript : MonoBehaviour
 {
     // Start is called before the first frame update
 
+    [Tooltip("Prefab to be instantiated when shooting (Projectile)")]
+    public GameObject projectilePrefab;
+    private float lastTimeFired = 0;
+    [Tooltip("How fast is the boss shooting")]
+    public float rateOfFire = 1;
+    public GameObject player;
+
     void Start()
     {
         
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        // if the fire button is pressed and we waited long enough since the last shot was fired, FIRE!
+        if ((lastTimeFired + 3 / rateOfFire) < Time.time)
+        {
+            lastTimeFired = Time.time;
+            FireTheBounce();
+        }
+    }
+
+    void FireTheBounce()
+    {
+        AudioSource.PlayClipAtPoint(bossSound, transform.position);
+
+        Instantiate(projectilePrefab, new Vector3(transform.position.x, transform.position.y + 5, transform.position.z), Quaternion.identity);
     }
 
 }
