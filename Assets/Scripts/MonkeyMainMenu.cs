49df69e0e1787f549304dfb6bb4a8867c3b579ca
Justin <yamadaj@carleton.edu>
2019-12-07 23:30:44 +0100

    menu sounds in

--- Assets/Scripts/MonkeyMainMenu.cs
+++ Assets/Scripts/MonkeyMainMenu.cs
@@ -9,6 +9,9 @@ public class MonkeyMainMenu : MonoBehaviour
     public TextMeshProUGUI raymondText;
     private Animator monkeyAnimator;
     private ArrayList nameList = new ArrayList();
+    public GameObject mainMenuMusic;
+    public GameObject camera;
+    public AudioClip ymcaSound;
     // Start is called before the first frame update
     void Start()
     {
@@ -34,21 +37,37 @@ public class MonkeyMainMenu : MonoBehaviour
         nameList.Add("Owen");
         nameList.Add("Justin");
         nameList.Add("Matt");
+
+        mainMenuMusic = GameObject.FindGameObjectWithTag("MainMenuSound");
+        camera = GameObject.FindGameObjectWithTag("MainCamera");
     }
 
     private void Update()
     {
         if (Input.GetKeyDown("r"))
         {
-                raymondText.text = (string) nameList[Random.Range(0, nameList.Count)];
+            raymondText.text = (string) nameList[Random.Range(0, nameList.Count)];
+            mainMenuMusic.gameObject.SetActive(true);
         }
 
         if (Input.GetKeyDown("y"))
         {
+            mainMenuMusic.gameObject.SetActive(false);
+            WaitCoroutine();
+            AudioSource.PlayClipAtPoint(ymcaSound, camera.transform.position);
+            
             raymondText.text = "YMCA";
             monkeyAnimator.SetTrigger("YMCA");
+
             
         }
+    }
+
+    IEnumerator WaitCoroutine()
+    {
+        Debug.Log("hi");
+        yield return new WaitForSeconds(3.0f);
+        mainMenuMusic.gameObject.SetActive(true);
     }
 }
 
