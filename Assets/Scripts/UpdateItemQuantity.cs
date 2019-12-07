9438e57a2d14dcff5fdaaf82cf45a2e77a3a070a
Justin Hahn <43224702+HahnJustin@users.noreply.github.com>
2019-12-07 18:45:24 +0100

    FINAL UI PUSH

---
+++ Assets/Scripts/UpdateItemQuantity.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UpdateItemQuantity : MonoBehaviour
+{
+    public TextMeshProUGUI jumpItemText;
+    public TextMeshProUGUI speedItemText;
+    public TextMeshProUGUI slowItemText;
+    public TextMeshProUGUI phaseItemText;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        jumpItemText.text = "" + singletonGameManager.Instance.numJumpItem;
+        speedItemText.text = "" + singletonGameManager.Instance.numSpeedItem;
+        slowItemText.text = "" + singletonGameManager.Instance.numSlowItem;
+        phaseItemText.text = "" + singletonGameManager.Instance.numPhaseItem;
+    }
+}
