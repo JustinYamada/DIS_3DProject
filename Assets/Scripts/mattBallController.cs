22ee4c3bac6bc16562ad7ef8535c3a207b104523
Justin <yamadaj@carleton.edu>
2019-12-07 13:18:10 +0100

    added beginning of boss mechanics

--- Assets/Scripts/mattBallController.cs
+++ Assets/Scripts/mattBallController.cs
@@ -34,6 +34,10 @@ public class mattBallController : MonoBehaviour
     private float verticalInput = 0f;
     private float horizontalInput = 0f;
 
+    public GameObject red;
+    public GameObject blue;
+    public GameObject green;
+
 
     /*
      * LOOK INTO SEPHAMORE WAIT FOR SIGNAL
@@ -223,6 +227,25 @@ public class mattBallController : MonoBehaviour
     {
         Time.timeScale = 0f;
         print("Unpaused");
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "MonkIdolRed")
+        {
+            Destroy(col.gameObject);
+            Destroy(red);
+        }
+        if (col.gameObject.tag == "MonkIdolBlue")
+        {
+            Destroy(col.gameObject);
+            Destroy(blue);
+        }
+        if (col.gameObject.tag == "MonkIdolGreen")
+        {
+            Destroy(col.gameObject);
+            Destroy(green);
+        }
     }
 
 
