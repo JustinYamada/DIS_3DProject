4d4b16e115dc559ecebe8fad371ba5c2c6724cb5
Owen <owengoldthwaite@gmail.com>
2019-12-07 17:43:08 +0100

    Time slow fix maybe???

--- Assets/Scripts/singletonGameManager.cs
+++ Assets/Scripts/singletonGameManager.cs
@@ -37,7 +37,6 @@ public class singletonGameManager : MonoBehaviour
     public int slowItemLevel = 1;
     public int slowItemUpgradePrice;
     public int slowItemPrice;
-    public float slowTimeMagnitude = 1.0f;
 
     public int[] scores = new int[8];
 
