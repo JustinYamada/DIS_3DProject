8386f01f1810a22b9a2a37eb41a432d8d22f73b9
Justin <yamadaj@carleton.edu>
2019-12-07 15:14:54 +0100

    added a sound folder

--- Assets/Scripts/ProjectileShooter.cs
+++ Assets/Scripts/ProjectileShooter.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class ProjectileShooter : MonoBehaviour
 {
-    [Tooltip("How fast is the projectile moving upwards")]
+    [Tooltip("How fast is the projectile moving")]
     public float speed = 10;
     [Tooltip("After how many seconds is the projectile destroyed")]
     public float lifeTime = 3;
 
-    [Tooltip("The direction the laster travels")]
+    [Tooltip("The direction the BounceObject travels")]
     public Vector3 direction;
     public GameObject player;
     Vector3 startPos;
@@ -21,29 +21,12 @@ public class ProjectileShooter : MonoBehaviour
         startPos = transform.position;
         player = GameObject.FindGameObjectWithTag("Player");
         rb = GetComponent<Rigidbody>();
-
-        //direction = player.transform.position;
         direction = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z);
-        // normalize direction so it does not impact the travel speed
-        //direction.Normalize();
-
-
-
-
-
-        // make the projectile rotate into the direction it is moving, math will be addressed in lecture 2
-        //float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90;
-        //transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        
-
     }
 
     void Update()
     {
-        //Debug.Log(player.transform.position.y);
         transform.position = Vector3.Lerp(startPos, direction, (3- lifeTime)/3);
-        print((3 - lifeTime) / 3);
-        //rb.AddForce(direction);
 
         lifeTime -= Time.unscaledDeltaTime;
         if (lifeTime <= 0)
